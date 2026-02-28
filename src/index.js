4fe30f337710776e69241d48cd96cb4d618a3c8d
Abdul Khader A <testingabu60@gmail.com>
2026-02-28 15:33:04 +0530

    Fix case-sensitive import for Vercel (Main.jsx)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import '../styles/index.css';
-import Main from './app/main';
+import Main from './app/Main.jsx';
 
 createRoot(document.getElementById('root')).render(React.createElement(Main));
