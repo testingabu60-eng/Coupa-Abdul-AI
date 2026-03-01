e845ec6b48d1b40e0fd34540fad6f8311dcfb37f
Abdul Khader A <testingabu60@gmail.com>
2026-03-01 16:50:42 +0530

    com

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,17 @@
+// src/app/App.jsx
+import React from 'react';
 import MainView from '../features/ui/views/MainView';
+import OafNavigation from '../features/navigation/OafNavigation';
 import '../../styles/app.css';
 
-const App = () => <MainView />;
+const App = () => (
+  <>
+    <MainView />
+    {/* Add the OAF Navigation panel anywhere you want on the page */}
+    <div style={{ padding: 20 }}>
+      <OafNavigation />
+    </div>
+  </>
+);
 
 export default App;
